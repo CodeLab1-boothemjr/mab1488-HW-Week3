35a4233caa327243be07fb6d6953c7e8ffc7835f
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2021-02-22 16:06:59 -0500

    re-initial commit

---
+++ ZombieRun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    private static int _currentLevel;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    public static void AdvanceCurrentLevel()
+    {
+        SceneManager.LoadScene(++_currentLevel);
+    }
+}
